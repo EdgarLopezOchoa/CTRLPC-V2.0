b467a20bcf8f213bbd54b8e5896ec908c35e6ad6
Edgar Lopez <Edgar1.el93@gmail.com>
2021-02-24 15:47:16 -0700

    Actualizacion del dia 24/02/2021
    -ScrollView
    -ActionBar

--- app/src/main/java/com/example/ctrlpc/MainActivity.java
+++ app/src/main/java/com/example/ctrlpc/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ctrlpc;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,21 +14,43 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_Articulos, R.id.navigation_Carrito, R.id.navigation_Cuenta,R.id.navigation_configuracion)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions,menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected( MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_search:
+
+                return true;
+            case R.id.action_settings:
+
+                return true;
+            default:
+                return  super.onOptionsItemSelected(item);
+
+        }
     }
 
 }
