cdecacc3c688917ae39babb379dd7c982cfc1b7a
Edgar Lopez <Edgar1.el93@gmail.com>
2021-02-24 17:08:44 -0700

    Actualizacion del dia 24/02/2021 a las 5:08
    -Splash Screen

---
+++ app/src/main/java/com/example/ctrlpc/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.ctrlpc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import static androidx.core.os.HandlerCompat.postDelayed;
+
+public class SplashScreen extends Activity {
+
+    private int DURATION_SPLACH = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+
+        }, DURATION_SPLACH);
+    }
+}
